9c5e3d8076a88c904feaf0f8e19b64d96eaa77fb
nageshdivatagi <nagesh.divatagi@gmail.com>
2023-02-12 22:34:15 +0530

    Nagesh initial commit

---
+++ src/test/java/com/knodtec/discovery/KpmsDiscoverySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knodtec.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpmsDiscoverySvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
